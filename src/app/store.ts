fe6ba58d1f4dc2c674835c9a6166a82d3f07f5a7
chiennq <chiennq@niw.com.vn>
2022-04-09 19:27:54 +0700

    change

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { cryptoNewsApi } from './../services/cryptoNewsApi';
+import { cryptoApi } from './../services/cryptoApi';
+import { configureStore, ThunkAction, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		[cryptoApi.reducerPath]: cryptoApi.reducer,
+		[cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			immutableCheck: { ignoredPaths: ['some.nested.path'] },
+			serializableCheck: { ignoredPaths: ['some.nested.path'] },
+		}).concat(cryptoApi.middleware, cryptoNewsApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
